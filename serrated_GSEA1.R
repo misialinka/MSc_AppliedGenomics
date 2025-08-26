894d832f79d7653645e01beb4c0040cc92780e58
Michalina Lubiatowska <mlubiat@gmail.com>
2025-08-26 13:21:54 +0200

    Add files via upload
    gene set enrichment analysis using hallmarks of cancer and selected pathways from KEGG database

--- serrated_GSEA1.R
+++ serrated_GSEA1.R
@@ -16,14 +16,12 @@ library(RColorBrewer) #package for colour palletes for graphs
 library(pheatmap) #package for heatmaps 
 library(magick) #package for image processing 
 
-#load the csv files with results of the DESeq2 analysis - output of DESEQ2 includes log2 fold changes in gene expression
+#load the csv files with results of the DESeq2 analysis - output of DESEQ2 includes log2 fold changes in gene expression. I am uploading lists for adenomas vs paired normal tissue (res_adenoma), serrated vs paired normal tissue (res_serrated), and comparison of serrated vs adenomas (res_comparison)
 res_adenoma <- read_csv("~/Desktop/serrated seq/RNAseq final/adenoma_polyp_vs_normal_final_results.csv")
 res_serrated <- read_csv("~/Desktop/serrated seq/RNAseq final/serrated_polyp_vs_normal_final_results.csv")
 res_comparison <- read_csv("~/Desktop/serrated seq/RNAseq final/change_from_normal_to_ser_vs_ade_results.csv")
-res_nvsn <- read_csv("~/Desktop/serrated seq/RNAseq final/normal_serrated_vs_adenoma_final_results.csv")
-res_pvsp <- read_csv("~/Desktop/serrated seq/RNAseq final/polyp_serrated_vs_adenoma_final_results.csv")
 
-#for GSEA analysis create a table including fold changes sorted in decreasing order and gene_ids. Make sure that the are no duplicates in gene names ---------
+#for GSEA analysis create a list including fold changes sorted in decreasing order and gene_ids. Make sure that the are no duplicates in gene names ---------
 ##for res_adenoma 
 res_adenoma <- res_adenoma |> rename(gene_id = ...1) |> as.data.frame()
 res_adenoma_list <- res_adenoma |> dplyr::select(gene_id, log2FoldChange) |> arrange(desc(log2FoldChange)) 
@@ -39,27 +37,17 @@ res_comparison <- res_comparison |> rename(gene_id = ...1)
 res_comparison_list <- res_comparison |> dplyr::select(log2FoldChange, gene_id) |> arrange(desc(log2FoldChange))
 geneList_comparison <- setNames(res_comparison_list$log2FoldChange, res_comparison_list$gene_id)
 
-##for res_nvsn
-res_nvsn <- res_nvsn |> rename(gene_id = ...1)
-res_nvsn_list <- res_nvsn |> dplyr::select(log2FoldChange, gene_id) |> arrange(desc(log2FoldChange))
-geneList_nvsn <- setNames(res_nvsn_list$log2FoldChange, res_nvsn_list$gene_id)
-
-##for res_pvsp
-res_pvsp <- res_pvsp |> rename(gene_id = ...1)
-res_pvsp_list <- res_pvsp |> dplyr::select(log2FoldChange, gene_id) |> arrange(desc(log2FoldChange))
-geneList_pvsp <- setNames(res_pvsp_list$log2FoldChange, res_pvsp_list$gene_id)
-
-#create gene enrichment results using MSigDb. From this database extract the category of gene set you are interested in. For example C5 includes gene ontology information, H includes clancer hallmarks gene sets. Extract the gene names from the same category that you have in gene lists. For currated gene sets C2 (includes kegg). C8 are cell types ---------
+#extract gene sets from MSigDb. Database H includes cancer hallmarks gene sets. Extract the gene names from the same category that you have in the gene lists. Database C2 includes KEGG gene sets, which can be filtered ---------
 H_t2g <- msigdbr(species = "Homo sapiens", category = "H") |>
   dplyr::select(gs_name, gene_symbol) |>
   mutate(gs_name = sub("^HALLMARK_", "", gs_name)) |>
   mutate(gs_name = gsub("_", " ", gs_name))
 head(H_t2g)
 
-C2_t2g <- msigdbr(species = "Homo sapiens", category = "C2") |> 
+C2_t2g <- msigdbr(species = "Homo sapiens", category = "C2") |>   
   dplyr::select(gs_name, gene_symbol) 
-
 head(C2_t2g)
+
 #filter KEGG pathways form currated signature C2 
 KEGG_t2g <- C2_t2g |> 
   filter(grepl(pattern = "^KEGG", gs_name)) |>
@@ -68,8 +56,8 @@ KEGG_t2g <- C2_t2g |>
   mutate(gs_name = sub("^MEDICUS_", "", gs_name)) |>
   mutate(gs_name = gsub("_", " ", gs_name))
 
-#comparisons in hallmarks ------
-#ade vs normal
+#conducting GSEA using hallmarks of cancer on adenomas, serrated polyps and comparison between them ------
+#hallmarks GSEA for ade vs normal
 set.seed(97)
 gse_hallmarks_adenoma <- GSEA(geneList_adenoma, TERM2GENE = H_t2g)
 gse_hallmarks_adenoma_dotplot <- dotplot(gse_hallmarks_adenoma, x = "NES", color = "p.adjust", showCategory=25) + 
@@ -77,7 +65,7 @@ gse_hallmarks_adenoma_dotplot <- dotplot(gse_hallmarks_adenoma, x = "NES", color
   lims(x=c(-3, 3)) +
   ggtitle("adenoma/ polyp vs. normal hallmarks")
 
-#serrated vs normal
+#hallmarks GSEA for serrated vs normal
 set.seed(256)
 gse_hallmarks_serrated <- GSEA(geneList_serrated, TERM2GENE = H_t2g)
 gse_hallmarks_serrated_dotplot <- dotplot(gse_hallmarks_serrated, x = "NES", showCategory=20) + 
@@ -85,14 +73,14 @@ gse_hallmarks_serrated_dotplot <- dotplot(gse_hallmarks_serrated, x = "NES", sho
   lims(x=c(-3, 3)) +
   ggtitle("serrated/ polyp vs. normal hallmarks") 
 
-#normal to serrated vs polyp 
+#hallmarks GSEA for normal to serrated vs polyp 
 gse_hallmarks_comparison <- GSEA(geneList_comparison, TERM2GENE = H_t2g)
 gse_hallmarks_comparison_dotplot <- dotplot(gse_hallmarks_comparison,  x = "NES", showCategory=25) + 
   ggtitle("normal to serrated vs. adenoma hallmarks") +
   theme_light() +
   lims(x=c(-3, 3))
 
-#shared doplot for serrated and adenomas 
+#selecting data neccessary to create a shared doplot for serrated and adenomas 
 dfH_adenoma <- as.data.frame(gse_hallmarks_adenoma_dotplot[["data"]])
 dfH_serrated <- as.data.frame(gse_hallmarks_serrated_dotplot[["data"]])
 dfH_comparison <- as.data.frame(gse_hallmarks_comparison_dotplot[["data"]])
@@ -105,6 +93,7 @@ combined_gsea_sig <- combined_gsea |>
 combined_gsea_sig$group <- as.factor(combined_gsea_sig$group)
 combined_gsea_sig$group <- relevel(combined_gsea_sig$group, ref = "Serrated vs normal")
 
+#plotting the significantly enriched hallmark pathways in adenomas, serrated polyps, and their comparisons
 ggplot(combined_gsea_sig, aes(x = group, y = Description, size = GeneRatio, color = NES)) +
   geom_point() +
   scale_color_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0 ) +
@@ -115,91 +104,7 @@ ggplot(combined_gsea_sig, aes(x = group, y = Description, size = GeneRatio, colo
        size = "GeneRatio",
        color = "NES")
 
-combined_gsea_comparison <- combined_gsea_sig |> filter(combined_gsea_sig$group == "Serrated vs adenoma")
-ggplot(combined_gsea_comparison, aes(x = NES, y = Description, size = -p.adjust, colour = NES)) +
-  geom_point() +
-  scale_color_gradient2(low = "blue", mid = "white", high = "red",  midpoint = 0 ) +
-  theme_minimal(base_size = 12) +
-  labs(title = "Cancer hallmarks serrated vs adenoma",
-       x = "Normalized Enrichment Score (NES)",
-       y = "Pathway",
-       color = "NES",
-       size = "Adjusted p-value") +
-  theme(strip.text = element_text(face = "bold"))
-
-
-#comparison in KEGG --------
-#adenoma vs normal
-gse_KEGG_adenoma <- GSEA(geneList_adenoma, TERM2GENE = KEGG_t2g)
-dotplot(gse_KEGG_adenoma, x = "NES", color = "p.adjust", showCategory=20) + 
-  theme_light() +
-  lims(x=c(-3, 3)) +
-  ggtitle("adenoma/ polyp vs. normal KEGG") 
-
-#serrated vs normal
-set.seed(256)
-gse_KEGG_serrated <- GSEA(geneList_serrated, TERM2GENE = KEGG_t2g)
-dotplot(gse_KEGG_serrated, x = "NES", showCategory=20) + 
-  theme_light() +
-  lims(x=c(-3, 3)) +
-  ggtitle("serrated/ polyp vs. normal KEGG") 
-
-#normal to serrated vs polyp 
-gse_KEGG_comparison <- GSEA(geneList_comparison, TERM2GENE = KEGG_t2g)
-dotplot(gse_KEGG_comparison,  x = "NES", showCategory=20) + 
-  ggtitle("normal to serrated vs. adenoma hallmarks") +
-  theme_light() +
-  lims(x=c(-3, 3))
-
-#normal serrated vs adenoma 
-gse_KEGG_nvsn <- GSEA(geneList_nvsn, TERM2GENE = KEGG_t2g)
-dotplot(gse_KEGG_nvsn,  x = "NES", showCategory=20) + 
-  ggtitle("normal in serrated vs. adenoma KEGG") +
-  theme_light() +
-  lims(x=c(-3, 3))
-
-#polyp serrated vs adenoma 
-gse_KEGG_pvsp <- GSEA(geneList_pvsp, TERM2GENE = KEGG_t2g)
-dotplot(gse_KEGG_pvsp,  x = "NES", showCategory=20) + 
-  ggtitle("serrated vs. adenoma polyp KEGG") +
-  theme_light() +
-  lims(x=c(-3, 3))
-
-#shared doplot for serrated and adenomas 
-dfKEGG_adenoma <- as.data.frame(gse_KEGG_adenoma@result)
-dfKEGG_serrated <- as.data.frame(gse_KEGG_serrated@result)
-dfKEGG_comparison <- as.data.frame(gse_KEGG_comparison@result)
-dfKEGG_adenoma$group <- "Adenoma vs normal"
-dfKEGG_serrated$group <- "Serrated vs normal"
-dfKEGG_comparison$group <- "Serrated vs adenoma"
-combined_KEGG <- rbind(dfKEGG_adenoma, dfKEGG_serrated, dfKEGG_comparison)
-combined_KEGG_sig <- combined_KEGG |>
-  filter(p.adjust < 0.05)
-combined_KEGG_sig$group <- as.factor(combined_KEGG_sig$group)
-combined_KEGG_sig$group <- relevel(combined_KEGG_sig$group, ref = "Serrated vs normal")
-
-ggplot(combined_KEGG_sig, aes(x = group, y = Description, size = -p.adjust, color = NES)) +
-  geom_point() +
-  scale_color_gradient(low = "red", high = "blue", trans = "reverse") +
-  theme_minimal() +
-  labs(title = "KEGG Adenoma vs Serrated",
-       x = "Group",
-       y = "KEGG Pathway",
-       size = "Adjusted p-value",
-       color = "NES")
-
-combined_KEGG_comparison <- combined_KEGG_sig |> filter(combined_KEGG_sig$group == "Serrated vs adenoma")
-ggplot(combined_KEGG_comparison, aes(x = NES, y = Description, color = p.adjust, size = abs(NES))) +
-  geom_point() +
-  scale_color_gradient(low = "red", high = "blue", trans = "reverse") +
-  theme_minimal(base_size = 12) +
-  labs(title = "Serrated vs adenoma GSEA",
-       x = "Normalized Enrichment Score (NES)",
-       y = "Pathway",
-       color = "Adjusted p-value",
-       size = "Count") +
-  theme(strip.text = element_text(face = "bold"))
-
+#extracting pathways of interest for enrichment analysis from KEGG database --------
 #verifying GSEA for antigen presentation --------
 antigen_t2g <- KEGG_t2g |>
   filter(grepl(pattern = "^ANTIGEN PROCESSING AND PRESENTATION$", gs_name))
@@ -217,7 +122,7 @@ gseaplot2(
 
 hla_adenoma_res <- dotplot(hla_adenoma)
 
-#for serrated lesions 
+#GSEA for antigen processing for serrated lesions 
 hla_serrated <- GSEA(geneList_serrated, 
                     TERM2GENE = antigen_t2g, 
                     minGSSize = 1,
@@ -229,7 +134,7 @@ gseaplot2(
 )
 hla_serrated_res <- dotplot(hla_serrated)
 
-#for serrated vs adenoma lesions 
+#GSEA for antigen processing for serrated vs adenoma lesions 
 hla_ser_vs_ade <- GSEA(geneList_comparison, 
                        TERM2GENE = antigen_t2g, 
                        minGSSize = 1,
@@ -242,7 +147,7 @@ gseaplot2(
 
 hla_comparison_res <- dotplot(hla_ser_vs_ade)
 
-#making a single graph for hla 
+#making a single dotplot for adenomas, serrated polyps and their comparion for GSEA for antigen processing
 hla_adenoma <- as.data.frame(hla_adenoma_res[["data"]])
 hla_serrated <- as.data.frame(hla_serrated_res[["data"]])
 hla_ser_vs_ade <- as.data.frame(hla_comparison_res[["data"]])
@@ -252,17 +157,6 @@ hla_ser_vs_ade$group <- "Serrated vs adenoma"
 hla <- rbind(hla_adenoma, hla_serrated, hla_ser_vs_ade)
 hla$group <- as.factor(hla$group)
 hla$group <- relevel(hla$group, ref = "Serrated vs normal")
-
-ggplot(hla, aes(x = NES, y = group, size = GeneRatio, color = NES)) +
-  geom_point(alpha = 0.8) +
-  scale_color_gradient2(low = "blue", mid = "white",  high = "red",  midpoint = 0 ) +
-  theme_minimal(base_size = 12) +
-  labs(
-    x     = "NES for Antigen Processing and Presentation",
-    y     = NULL,
-    size  = "Gene Ratio",
-    color = "NES") +
-  theme(strip.text = element_text(face = "bold"))
 
 ggplot(hla, aes(x = group, y = Description, size = GeneRatio, color = NES)) +
   geom_point(alpha = 0.8) +
@@ -275,11 +169,11 @@ ggplot(hla, aes(x = group, y = Description, size = GeneRatio, color = NES)) +
     color = "NES") +
   theme(strip.text = element_text(face = "bold"))
 
-#verifying GSEA for PDL1/PD and CTLA4 -------
+#verifying GSEA for PDL1/PD and CTLA4 (immune checkpoint) signalling from KEGG databases-------
 icb_t2g <- KEGG_t2g |>
   filter(gs_name == "CD80 CD86 CTLA4 PP2A SIGNALING PATHWAY" | gs_name == "PDL PD1 SHP PI3K SIGNALING PATHWAY")
 
-#GSEA for PDL1/PD and CTLA4  for adenomas
+#GSEA for PDL1/PD and CTLA4 signalling for adenomas
 icb_adenoma <- GSEA(geneList_adenoma, 
                     TERM2GENE = icb_t2g, 
                     minGSSize = 1,
@@ -292,7 +186,7 @@ gseaplot2(
 
 icb_adenoma_res <- dotplot(icb_adenoma)
 
-#PDL1/PD and CTLA4 enrichemnt for serrated lesions 
+#GSEA for PDL1/PD and CTLA4 signalling for serrated lesions 
 icb_serrated <- GSEA(geneList_serrated, 
                      TERM2GENE = icb_t2g, 
                      minGSSize = 1,
@@ -305,7 +199,7 @@ gseaplot2(
 
 icb_serrated_res <- dotplot(icb_serrated)
 
-#PDL1/PD and CTLA4 enrichment for serrated vs adenoma lesions 
+#GSEA for PDL1/PD and CTLA4 signalling for serrated vs adenoma lesions 
 icb_ser_vs_ade <- GSEA(geneList_comparison, 
                        TERM2GENE = icb_t2g, 
                        minGSSize = 1,
@@ -318,7 +212,7 @@ gseaplot2(
 
 icb_comparison_res <- dotplot(icb_ser_vs_ade)
 
-#making a single graph for hla 
+#making a single dotplot for adenomas, serrated polyps and their comparion for GSEA for PDL1/PD and CTLA4 signalling
 icb_adenoma <- as.data.frame(icb_adenoma_res[["data"]])
 icb_serrated <- as.data.frame(icb_serrated_res[["data"]])
 icb_comparison <- as.data.frame(icb_comparison_res[["data"]])
@@ -328,18 +222,6 @@ icb_comparison$group <- "Serrated vs adenoma"
 icb <- rbind(icb_adenoma, icb_serrated, icb_comparison)
 icb$group <- as.factor(icb$group)
 icb$group <- relevel(icb$group, ref = "Serrated vs normal")
-
-ggplot(icb, aes(x = NES, y = group, size = GeneRatio, color = NES)) +
-  geom_point(alpha = 0.8) +
-  scale_color_gradient2(low = "blue", mid = "white",  high = "red",  midpoint = 0 ) +
-  theme_minimal(base_size = 12) +
-  labs(
-    x     = "NES",
-    y     = NULL,
-    size  = "Gene Ratio",
-    color = "NES") +
-  theme(strip.text = element_text(face = "bold")) +
-  facet_wrap(~ID, nrow = 2)
 
 ggplot(icb, aes(x = group, y = Description, size = GeneRatio, color = NES)) +
   geom_point(alpha = 0.8) +
